c9614700ca9cdee84474b29db218730d7d985d18
Aayush Bhatt <aayushbhatt20@gmail.com>
2021-06-30 23:22:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,6 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 }
 
-document.addEventListener("keypress", function(event) {
-
-  makeSound(event.key);
-
-  buttonAnimation(event.key);
-
-});
 
 
 function makeSound(key) {
